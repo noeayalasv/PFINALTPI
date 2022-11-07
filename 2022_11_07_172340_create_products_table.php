487298d6a1f48f342907a43fe9ba09355acf1e36
Noe <112187368+noeayalasv@users.noreply.github.com>
2022-11-07 11:55:05 -0600

    Add files via upload
    Migración de productos.

---
+++ 2022_11_07_172340_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name_product');
+            $table->string('category');
+            $table->string('image_product');
+            $table->string('supplier');
+            $table->integer('stock');
+            $table->decimal('cost',4,2);
+            $table->decimal('price',4,2);
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
